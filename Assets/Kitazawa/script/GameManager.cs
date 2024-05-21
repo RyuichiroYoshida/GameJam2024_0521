4d3e1bf23258c724b3c6eb0a1f22626f200f261b
kitazawa <124682835+NinNin77@users.noreply.github.com>
2024-05-21 12:45:08 +0900

    おためし

---
+++ Assets/Kitazawa/script/GameManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState
+{
+    Title,
+    Prepare,
+    InGame,
+    Dead,
+    Result
+}
+
+public class GameManager : MonoBehaviour
+{
+    // これ
+    public static GameManager thisInstance;
+
+    // 現在のゲームステータス
+    private GameState currentGameState;
+
+
+    // 例
+    public Text label;
+    public Button button;
+
+    void Awake()
+    {
+        thisInstance = this;
+        SetCurrentState(GameState.Title);
+    }
+
+
+    // 外からこのメソッドを使って状態を変更
+    public void SetCurrentState(GameState state)
+    {
+        currentGameState = state;
+        OnGameStateChanged(currentGameState);
+    }
+
+    // 状態が変わったら何をするか -> メソッドを実行
+    void OnGameStateChanged(GameState state)
+    {
+        switch (state)
+        {
+            case GameState.Title:
+                StartAction();
+                break;
+            case GameState.Prepare:
+                StartCoroutine(PrepareCoroutine());
+                break;
+            case GameState.InGame:
+                InGameAction();
+                break;
+            case GameState.Dead:
+                DeadAction();
+                break;
+            case GameState.Result:
+                EndAction();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Startになったときの処理
+    void StartAction()
+    {
+    }
+
+    // Prepareになったときの処理
+    IEnumerator PrepareCoroutine() //なにこれ。コールチンってなに？
+    {
+        label.text = "3";
+        yield return new WaitForSeconds(1);
+        label.text = "2";
+        yield return new WaitForSeconds(1);
+        label.text = "1";
+        yield return new WaitForSeconds(1);
+        label.text = "";
+        SetCurrentState(GameState.InGame);
+    }
+    // InGameになったときの処理
+    void InGameAction()
+    {
+        label.text = "ゲーム中";
+
+    }
+    // Deadになったときの処理
+    void DeadAction()
+    {
+    }
+    // Resultになったときの処理
+    void EndAction()
+    {
+    }
+}
+
