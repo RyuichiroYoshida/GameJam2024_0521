682fd406650153c2dc5f0a6453780586fa1af74b
Enulu <enuarle8@icloud.com>
2024-05-22 11:15:52 +0900

    [Add]testSceneMain,[Fix]StartFade

--- Assets/Noda/NScript/StartFade.cs
+++ Assets/Noda/NScript/StartFade.cs
@@ -7,8 +7,11 @@ using DG.Tweening;
 public class StartFade : MonoBehaviour
 {
     [SerializeField]UnityEngine.UI.Image image;
+    GameManager _gameManager;
     void Start()
     {
+        _gameManager = FindObjectOfType<GameManager>();
+        _gameManager.SetGameState(GameState.InGame);
         this.image.DOFade(endValue: 0f, duration: 1f);
         Invoke("Destroy", 1f);
     }
